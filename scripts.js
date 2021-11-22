bf331fb557ac81a9c97b8b2f3e59f922b3bc3974
Evgenii <evg.kuzmin@gmail.com>
2021-11-22 17:17:55 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+const focusTime = 25 * 60 * 1000; // 25 minutes
+const storageItem = 'startTimer';
+let remainingTimeDiv;
+
+
+window.onload = onLoad();
+
+function onLoad(){
+    remainingTimeDiv = document.querySelector('#remainingTime');
+}
+
+
+function formatMinSec(value){
+    if (value>9)
+        return value;
+    else
+        return '0'+value;
+}
+
+function millisToMinutes(millis){
+    const minutes = Math.floor(millis / 60000);
+    const seconds = Math.floor((millis - minutes * 60000) / 1000);
+    return `${formatMinSec(minutes)}:${formatMinSec(seconds)}`;
+}
+
+function count(){
+    let startTime = localStorage.getItem(storageItem);
+    let remainingTime = focusTime - (Date.now() - startTime);
+    if (remainingTime<=0)
+    // done!
+        console.log('completed!');
+    else{
+        remainingTimeDiv.innerHTML = millisToMinutes(remainingTime);
+        setTimeout(count, 1000);
+    }
+}
+
+function startCounting(evt){
+    localStorage.setItem(storageItem, Date.now());
+    count();
+}
