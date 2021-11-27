12eaec9e5eda1559ad4a750173e6ab119b060581
Kmm7 <13196402+kmm7@users.noreply.github.com>
2021-11-27 09:58:08 +0100

    Tab switching added

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,20 @@
-const focusTime = 25 * 60 * 1000; // 25 minutes
+let activeTimer;
+const timers =     [
+    {
+        name: 'focus',
+        time: 2 * 60 * 1000
+    },
+    {
+        name: 'short-break',
+        time: 5 * 60 * 1000
+    },
+    {
+        name: 'long-break',
+        time: 15 * 60 * 1000
+    }
+];
+let runningTimer;
+
 const storageItem = 'startTimer';
 let remainingTimeDiv;
 
@@ -6,9 +22,38 @@ let remainingTimeDiv;
 window.onload = onLoad();
 
 function onLoad(){
+    activeTimer = 'focus';
+    var tabs = document.querySelectorAll('nav div');
+    tabs.forEach(tab=>tab.addEventListener('click', ()=>{
+        if (activeTimer !== tab.dataset.timerName){
+            clearTimeout(runningTimer);
+
+            tabs.forEach(t=>t.classList.remove('highlited'));
+            activeTimer = tab.dataset.timerName;
+            tab.classList.add('highlited');
+            
+            showCounter(getActiveTimerDuration());
+        }
+    }));
+
     remainingTimeDiv = document.querySelector('#remainingTime');
+
+    showCounter(getActiveTimerDuration());
+    askForNotificationPermission();
 }
 
+function getActiveTimerDuration(){
+    const activeTimerObject = timers.find(p=>p.name === activeTimer);
+    return activeTimerObject.time;
+}
+function askForNotificationPermission(){
+    if (!("Notification" in window))
+    return;
+
+    if (["granted", "denied"].indexOf(Notification.permission) == -1) {
+        Notification.requestPermission();
+    }
+}
 
 function formatMinSec(value){
     if (value>9)
@@ -25,15 +70,28 @@ function millisToMinutes(millis){
 
 function count(){
     let startTime = localStorage.getItem(storageItem);
-    let remainingTime = focusTime - (Date.now() - startTime);
+    let remainingTime = getActiveTimerDuration() - (Date.now() - startTime);
     if (remainingTime<=0)
     // done!
-        console.log('completed!');
+        sendNotification();
     else{
-        let remainingTimeStr = millisToMinutes(remainingTime);
-        document.title = remainingTimeStr;
-        remainingTimeDiv.innerHTML = remainingTimeStr;
-        setTimeout(count, 1000);
+        showCounter(remainingTime);
+        runningTimer = setTimeout(count, 1000);
+    }
+}
+
+function showCounter(remainingTime){
+    let remainingTimeStr = millisToMinutes(remainingTime);
+    document.title = `${activeTimer} ${remainingTimeStr}`;
+    remainingTimeDiv.innerHTML = remainingTimeStr;
+}
+
+function sendNotification(){
+    if (!("Notification" in window))
+        return;
+
+    if (Notification.permission === "granted") {
+        var notification = new Notification("Timer ended!");
     }
 }
 
